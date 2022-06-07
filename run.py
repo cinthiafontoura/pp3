6c9b6517fc3d63750e4ef21c7dea0ef395a28a59
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-06-07 19:30:57 +0000

    Add choose_move funcion to ask the player and create the computer moves

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-print("""
-                           WELCOME TO THE ROCK PAPER SCISSORS GAME!
+import random
+
+print("""                  WELCOME TO THE ROCK PAPER SCISSORS GAME!
     _______                 
 ---'   ____)            This game is played by children and adults and is
       (_____)       popular all over the world. Apart from being a game played
@@ -20,5 +21,50 @@ print("""
        __________)      The rock will beat scissors every time but will be
       (____)        beaten by paper. Paper will beat rock but will be beaten by
 ---.__(___)         scissors in no time.      
-                    
+
 """)  
+
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+def choose_move():
+    """
+    Ask to the player choose between rock, paper, and scissors by enter a number. 
+    Create a random move to the computer.
+    """
+    user_move = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+    computer_move = random.randint(0,2)
+
+    return user_move, computer_move
+
+moves = choose_move()
+print(moves)
+print(f"YOU choose: {moves[0]}")
+print(f"The COMPUTER choose: {moves[1]}")
